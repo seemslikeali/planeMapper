f4b639c86139cb62f77f4f36027a463a10b3cbbd
Ali Shakeel <50228096+seemslikeali@users.noreply.github.com>
2023-11-22 18:36:28 +0000

    settingsupdate

--- planeMapper/wsgi.py
+++ planeMapper/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planeMapper.settings')
 
 application = get_wsgi_application()
+
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
