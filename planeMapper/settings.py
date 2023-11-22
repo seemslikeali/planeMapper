993124744dba43f9711f1c7485604bc114a4936b
Ali Shakeel <50228096+seemslikeali@users.noreply.github.com>
2023-11-22 13:29:16 -0500

    Update settings.py

--- planeMapper/settings.py
+++ planeMapper/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zluyo+tk=ifcbi*_6bzde+=3fc4om-i7oo_##5d#0csv7r)=1+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
